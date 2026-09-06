d41393435059c17e7bdc04823f57fd55dff6731d
minayakop <161607980+minayakop@users.noreply.github.com>
2026-09-06 18:47:26 +0300

    Add CSRF trusted origins for security

--- core/settings.py
+++ core/settings.py
@@ -8,6 +8,7 @@ SECRET_KEY = config('SECRET_KEY', default='django-insecure-rq^11u6hzf)2e#y^bm3q*
 DEBUG = config('DEBUG', default=True, cast=bool)
 
 ALLOWED_HOSTS = ['mina2.pythonanywhere.com', 'localhost', '127.0.0.1']
+CSRF_TRUSTED_ORIGINS = ['https://mina2.pythonanywhere.com']
 
 INSTALLED_APPS = [
     'django.contrib.admin',
